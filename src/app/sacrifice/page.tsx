2c26d9466971595cc651ad90539702cb677abfbe
Teddylemon1020 <hakkiroan@gmail.com>
2025-08-19 21:32:37 +0800

    feat: setup

---
+++ src/app/sacrifice/page.tsx
@@ -0,0 +1,1 @@
+//correlated to current events can be added to page fetch
---
+++ src/components/error/ComingSoon.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ComingSoon() {
+  //insert iamge of king mated
+  return <div>ComingSoon</div>;
+}
+
+export default ComingSoon;
